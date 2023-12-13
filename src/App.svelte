d238f49f042ab95555a9f5b0b99db4d8ba850889
Monika <monika.rai@madebyaura.com>
2023-12-13 19:07:40 +0530

    checkmark functionality added

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,23 @@
 <script>
   import { onMount } from "svelte";
-  import { writable } from "svelte/store";
   import options from "./data.json";
 
   let tabOptions = options.tabOptions;
-  let showCheckmark = false;
   let activeTab = "Home";
+  let selectedOption = null;
 
   function openPage(pageName) {
     activeTab = pageName;
-
-    // Reset the checkmark after 2 seconds
-    setTimeout(() => {
-      showCheckmark = false;
-    }, 2000);
-
     console.log(pageName);
+  }
+
+  function handleRadioChange(event) {
+    selectedOption = event.target.value;
+
+    // Set the checkmark to appear
+    setTimeout(() => {
+      selectedOption = null; // Reset the selectedOption after 2 seconds
+    }, 2000);
   }
 
   // Automatically click the default tab on component mount
@@ -36,15 +38,36 @@
 
   {#each tabOptions as tabOption}
     {#if activeTab === tabOption.type}
-      <div class="tabcontent text-black block p-10 h-full">
+      <div id={tabOption.type} class="tabcontent text-black block p-10 h-full">
         <h3>{tabOption.type}</h3>
 
-        <div>
-          <label>
-            <input type="radio" name="option" value="option1" /> Option 1
-          </label><br />
-          <label>
-            <input type="radio" name="option" value="option2" /> Option 2
+        <!-- Add radio buttons here -->
+        <div class="radio-container flex flex-col my-3">
+          <label class="radio-option flex items-center mb-2">
+            <input
+              type="radio"
+              name="option"
+              value="Option 1"
+              on:change={handleRadioChange}
+              checked={selectedOption === "Option 1"}
+            />Option 1
+            {#if selectedOption === "Option 1"}<span
+                class="checkmark mr-2 text-[#008000] text-base font-extrabold"
+                >&#10003;</span
+              >{/if}
+          </label>
+          <label class="radio-option">
+            <input
+              type="radio"
+              name="option"
+              value="Option 2"
+              on:change={handleRadioChange}
+              checked={selectedOption === "Option 2"}
+            />Option 2
+            {#if selectedOption === "Option 2"}<span
+                class="checkmark mr-2 text-[#008000] text-base font-extrabold"
+                >&#10003;</span
+              >{/if}
           </label>
         </div>
       </div>
