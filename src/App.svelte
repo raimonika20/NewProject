04ada8121288715dd9f2220f207a48c3821b1070
Monika <monika.rai@madebyaura.com>
2023-12-13 18:44:59 +0530

    Radio buttons added

--- src/App.svelte
+++ src/App.svelte
@@ -4,56 +4,49 @@
   import options from "./data.json";
 
   let tabOptions = options.tabOptions;
-
+  let showCheckmark = false;
   let activeTab = "Home";
-  let toggleStates = {};
-
-  tabOptions.forEach((tabOption) => {
-    toggleStates[tabOption.id] = writable(false);
-    toggleStates[`show${tabOption.type}Tick`] = writable(false);
-  });
 
   function openPage(pageName) {
     activeTab = pageName;
+
+    // Reset the checkmark after 2 seconds
+    setTimeout(() => {
+      showCheckmark = false;
+    }, 2000);
+
     console.log(pageName);
   }
 
   // Automatically click the default tab on component mount
   onMount(() => {
-    document.getElementById("defaultOpen").click();
+    openPage(activeTab);
   });
-
-  // Subscribe to the toggle change and show tick when it's ON
-  $: if (toggleStates[`${activeTab.toLowerCase()}Toggle`]) {
-    toggleStates[`show${activeTab}Tick`].set(true);
-
-    // Clear the existing timeout to avoid multiple setTimeouts
-    setTimeout(() => toggleStates[`show${activeTab}Tick`].set(false), 2000);
-  }
 </script>
 
 <main>
-  {#each tabOptions as tabOption}
-    <button
-      class="tablink bg-[#395f79] font-semibold text-white float-left cursor-pointer p-3 text-lg w-1/3"
-      on:click={() => openPage(tabOption.type)}>{tabOption.type}</button
-    >
-  {/each}
+  <div class="container flex items-center justify-center">
+    {#each tabOptions as tabOption}
+      <button
+        class="tablink bg-[#395f79] font-semibold text-white float-left cursor-pointer p-3 text-lg w-full"
+        on:click={() => openPage(tabOption.type)}>{tabOption.type}</button
+      >
+    {/each}
+  </div>
 
   {#each tabOptions as tabOption}
     {#if activeTab === tabOption.type}
-      <div id={tabOption.type} class="tabcontent text-black block p-20 h-full">
+      <div class="tabcontent text-black block p-10 h-full">
         <h3>{tabOption.type}</h3>
-        <label class="switch">
-          <input
-            type="checkbox"
-            bind:checked={toggleStates[`${tabOption.id}Toggle`]}
-          />
-          <span class="slider round"></span>
-        </label>
-        {#if toggleStates[`show${tabOption.type}Tick`] && toggleStates[`${tabOption.id}Toggle`]}
-          <span class="tick font-bold text-green-600 mx-2.5">✓</span>
-        {/if}
+
+        <div>
+          <label>
+            <input type="radio" name="option" value="option1" /> Option 1
+          </label><br />
+          <label>
+            <input type="radio" name="option" value="option2" /> Option 2
+          </label>
+        </div>
       </div>
     {/if}
   {/each}
