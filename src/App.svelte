5d9d5ea11d7ba41ca56e8817a381ed71bdca2995
Monika <monika.rai@madebyaura.com>
2023-12-15 18:35:01 +0530

    Select tag & Input field added

--- src/App.svelte
+++ src/App.svelte
@@ -4,19 +4,25 @@
 
   let tabOptions = tabs.tabOptions;
   let activeTab = "Home";
-  let selectedOption = null;
+
+  // Maintain separate states for toggle switches
+  let toggleSwitchStates = {};
+  let showCheckmark = false;
 
   function openPage(pageName) {
     activeTab = pageName;
     console.log(pageName);
   }
 
-  function handleRadioChange(event) {
-    selectedOption = event.target.value;
+  function toggleSwitchChanged(tabType, event) {
+    toggleSwitchStates[tabType] = event.target.checked;
 
-    // Set the checkmark to appear
+    // Set showCheckmark to true if the current tab's toggle switch is checked
+    showCheckmark = toggleSwitchStates[activeTab];
+
+    // Reset the checkmark after 2 seconds
     setTimeout(() => {
-      selectedOption = null; // Reset the selectedOption after 2 seconds
+      showCheckmark = false;
     }, 2000);
   }
 
@@ -29,7 +35,7 @@
   <div class="container flex items-center justify-center">
     {#each tabOptions as tabOption}
       <button
-        class="tablink bg-[#395f79] font-semibold text-white float-left cursor-pointer p-3 text-lg w-full"
+        class="tablink bg-[#a0bfd4] font-semibold text-white float-left cursor-pointer p-3 text-lg w-full"
         on:click={() => openPage(tabOption.type)}>{tabOption.type}</button
       >
     {/each}
@@ -37,46 +43,54 @@
 
   {#each tabOptions as tabOption}
     {#if activeTab === tabOption.type}
-      <div class="tabcontent text-black block p-10 h-full">
-        <h3>{tabOption.type}</h3>
+      <div class="tab-container flex flex-col p-8 m-3">
+        <div class="tabcontent text-black block flex items-center mb-2 h-full">
+          <h3 class="px-2">{tabOption.type}</h3>
 
-        <!-- Add radio buttons here -->
-        <div class="radio-container flex flex-col my-3">
-          <label class="radio-option flex items-center mb-2">
+          <!-- Toggle Switch -->
+          <label class="toggle-switch">
             <input
-              type="radio"
-              name="option"
-              value="Option 1"
-              on:change={handleRadioChange}
-              checked={selectedOption === "Option 1"}
-            />{tabOption.option1}
-            {#if selectedOption === "Option 1"}<span
-                class="checkmark mr-2 text-[#008000] text-base font-extrabold"
-                ><img
-                  class="checkmark-img w-5 h-5"
-                  src="src/assets/accept.png"
-                  alt=""
-                /></span
-              >{/if}
+              type="checkbox"
+              bind:checked={toggleSwitchStates[tabOption.type]}
+              on:change={(event) => toggleSwitchChanged(tabOption.type, event)}
+            />
+            <span class="toggle-switch-slider"></span>
           </label>
 
-          <label class="radio-option flex items-center mb-2">
-            <input
-              type="radio"
-              name="option"
-              value="Option 2"
-              on:change={handleRadioChange}
-              checked={selectedOption === "Option 2"}
-            />{tabOption.option2}
-            {#if selectedOption === "Option 2"}<span
-                class="checkmark mr-1 text-[#008000] text-base font-extrabold"
-                ><img
-                  class="checkmark-img w-5 h-5"
-                  src="src/assets/accept.png"
-                  alt=""
-                /></span
-              >{/if}
-          </label>
+          <!-- Checkmark -->
+          {#if showCheckmark}
+            <span class="checkmark px-2 text-base"
+              ><img
+                class="checkmark-img w-4 h-4"
+                src="src/assets/accept.png"
+                alt=""
+              /></span
+            >{/if}
+        </div>
+
+        <div class="tab-select flex flex-row block w-60 py-4">
+          <label for="pet-select" class="mr-4">Your Options</label>
+
+          <select
+            name="pets"
+            id="pet-select"
+            class="select-options border border-solid border-[#1e3d52] bg-[#a0bfd46a] text-[#1e3d52]"
+          >
+            <option value="">--Please choose an option--</option>
+
+            {#each tabOption.options as selectOption}
+              <option value="">{selectOption}</option>
+            {/each}
+          </select>
+        </div>
+
+        <div class="tab-input flex flex-row block w-50">
+          <label for="pet-select" class="pr-4">Write something:</label>
+          <input
+            type="text"
+            class="select-input"
+            placeholder="Write something"
+          />
         </div>
       </div>
     {/if}
